edf65289aefd206726a406720e25b1c2c89b923d
nikolay-ignis <nikolay.pallo@ignis-tech.ru>
2026-07-07 10:01:15 +0300

    работает функция удаления

--- AVLTree.h
+++ AVLTree.h
@@ -90,130 +90,15 @@ class AVL{
     {
         return find_in(val,head);
     }
-
-    // bool erase(const T& val)
-    // {
-    //     if(!head)
-    //     {
-    //         return false;
-    //     }
-    //     Node<T>* node = find(val);
-    //     if(!node)
-    //     {
-    //         return false;
-    //     }
-    //     if(node->left && node->right)
-    //     {
-    //         Node<T>* newHead = findLeft(node->right);
-    //         Node<T>* rightTemp = newHead->right;
-    //         Node<T>* parentTemp = newHead->parent == node ? newHead : newHead->parent;
-    //         insertInstead(newHead,node);
-    //         if(parentTemp == newHead)
-    //         {
-    //             parentTemp->right = rightTemp;    
-    //         }
-    //         else
-    //         {
-    //             parentTemp->left = rightTemp;
-    //         }
-    //         if(rightTemp)
-    //         {
-    //             rightTemp->parent = parentTemp;
-    //         }
-    //     }
-    //     else if(node->left)
-    //     {
-    //         if(node->parent)
-    //         {
-    //             if(node->parent->left == node)
-    //             {
-    //                 node->parent->left = node->left;
-    //             }
-    //             else
-    //             {
-    //                 node->parent->right = node->left;
-    //             }
-    //             node->left->parent = node->parent;
-    //         }
-    //         else
-    //         {
-    //             head = node->left;
-    //             node->left->parent = nullptr;
-    //         }
-    //     }
-    //     else if(node->right)
-    //     {
-    //         if(node->parent)
-    //         {
-    //             if(node->parent->left == node)
-    //             {
-    //                 node->parent->left = node->right;
-    //             }
-    //             else
-    //             {
-    //                 node->parent->right = node->right;
-    //             }
-    //             node->right->parent = node->parent;
-    //         }
-    //         else
-    //         {
-    //             head = node->right;
-    //             node->right->parent = nullptr;
-    //         }
-    //     }
-    //     else
-    //     {
-    //         if(node->parent)
-    //         {
-    //             if(node->parent->left == node)
-    //             {
-    //                 node->parent->left = nullptr;
-    //             }
-    //             else
-    //             {
-    //                 node->parent->right = nullptr;
-    //             }
-    //         }
-    //         else
-    //         {
-    //             head = nullptr;
-    //         }
-    //     }
-    //     delete node;
-    //     return true;
-    // }
-    
-    void erase(Node<T>*& root, Node<T>* z)
+    bool erase(const T& val)
     {
-        if (z->left == nullptr)
+        Node<T>* node = find(val);
+        if(!node)
         {
-            transplant(root, z, z->right);
-            delete z;
+            return false;
         }
-        else if (z->right == nullptr)
-        {
-            transplant(root, z, z->left);
-            delete z;
-        }
-        else
-        {
-            Node<T>* y = findLeft(z->right);
-
-            if (y->parent != z)
-            {
-                transplant(root, y, y->right);
-
-                y->right = z->right;
-                y->right->parent = y;
-            }
-
-            transplant(root, z, y);
-
-            y->left = z->left;
-            y->left->parent = y;
-
-            delete z;
-        }
+        erase_node(head,node);
+        return true;
     }
 
     ~AVL(){
@@ -281,76 +166,6 @@ class AVL{
     {
         return node->left ? findLeft(node->left) : node;
     }
-    void insertInstead(Node<T>* insertNode, Node<T>* insteadNode)
-    {
-        if(insteadNode == insertNode)
-        {
-            return;
-        }
-
-        //Если есть родитель, то мы разъединяем с ним связь
-        if(insertNode->parent)
-        {
-            //если родитель это тот с кем мы меняемся то удаляем только у нас связь
-            //так как связь у insteadNode еще понадобитсья
-            if(insertNode->parent != insteadNode)
-            {
-                if(insertNode->parent->left == insertNode)
-                {
-                    insertNode->parent->left = nullptr;
-                }
-                else
-                {
-                    insertNode->parent->right = nullptr;
-                }
-            }
-            insertNode->parent = nullptr;
-        }
-
-
-        //отсоединяем родительские связи у потомков
-        if(insertNode->left)
-        {
-            insertNode->left->parent = nullptr;
-        }
-        if(insertNode->right)
-        {
-            insertNode->right->parent = nullptr;   
-        }
-
-
-
-        insertNode->left = insteadNode->left == insertNode ? nullptr : insteadNode->left;
-        insertNode->right = insteadNode->right == insertNode ? nullptr : insteadNode->right;
-
-        if(insertNode->left)
-        {
-            insertNode->left->parent = insertNode;
-        }
-
-        if(insertNode->right)
-        {
-            insertNode->right->parent = insertNode;
-        }
-
-        if(insteadNode->parent)
-        {
-            if(insteadNode->parent->left == insteadNode)
-            {
-                insteadNode->parent->left = insertNode;
-            }
-            else
-            {
-                insteadNode->parent->right = insertNode;
-            }
-        }
-        else
-        {
-            head = insertNode;
-        }
-        //delete insteadNode;
-    }
-
     void transplant(Node<T>*& root, Node<T>* u, Node<T>* v)
     {
         if (u->parent == nullptr)
@@ -362,6 +177,38 @@ class AVL{
 
         if (v != nullptr)
             v->parent = u->parent;
+    }
+    void erase_node(Node<T>*& root, Node<T>* z)
+    {
+        if (z->left == nullptr)
+        {
+            transplant(root, z, z->right);
+            delete z;
+        }
+        else if (z->right == nullptr)
+        {
+            transplant(root, z, z->left);
+            delete z;
+        }
+        else
+        {
+            Node<T>* y = findLeft(z->right);
+
+            if (y->parent != z)
+            {
+                transplant(root, y, y->right);
+
+                y->right = z->right;
+                y->right->parent = y;
+            }
+
+            transplant(root, z, y);
+
+            y->left = z->left;
+            y->left->parent = y;
+
+            delete z;
+        }
     }
 };
 
